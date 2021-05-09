254410aba7eb3e4d3f31fb726a9a8813f5167f5c
Tommy Martin <tomdmartin4@gmail.com>
2021-05-09 19:18:24 -0400

    Pulled out Azure Storage into its own repo (from commit 608768edc093dfbf008b36085e6df28999de9e49)

---
+++ Package.swift
@@ -0,0 +1,38 @@
+import PackageDescription
+
+let package = Package(
+    name: "AzureStorageBlob",
+    platforms: [
+        .macOS(.v10_15), .iOS(.v12)
+    ],
+    products: [
+        .library(name: "AzureStorageBlob", targets: ["AzureStorageBlob"])
+    ],
+    dependencies: [
+        .package(name: "AzureCore", url: "https://github.com/Azure/SwiftPM-AzureCore.git", from: "1.0.0-beta.12")
+    ],
+    targets: [
+        // Build targets
+        .target(
+            name: "AzureStorageBlob",
+            dependencies: ["AzureCore"],
+            path: "Source",
+            exclude: [
+                "README.md",
+                "Tests",
+                "Source/Supporting Files",
+                "LICENSE"
+            ]
+        ),
+        // Test targets
+        .testTarget(
+            name: "AzureStorageBlobTests",
+            dependencies: ["AzureStorageBlob"],
+            path: "Tests",
+            exclude: [
+                "Info.plist",
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
